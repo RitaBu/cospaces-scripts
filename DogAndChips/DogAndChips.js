4407e992a7ff5b711780b36dac1a42b172239754
Pavel Kakolin <pavel.kakolin@jetbrains.com>
2016-08-26 16:21:23 +0200

    "dog and chips" script

---
+++ DogAndChips/DogAndChips.js
@@ -0,0 +1,29 @@
+var size = 10;
+
+var items = DX.items();
+for (var k = 0; k < items.length; k++) {
+  items[k].remove();
+}
+
+var dogId = DX.createItem("LP_Dog", 0, 0, 1);
+var dog = DX.item(dogId);
+dog.setColor(162, 42, 42);
+
+function jump(x, y) {
+  return function() {
+    dog.throwTo(x, y, 0, 1, function() {
+      dog.animateToState("StandHowl");
+      dog.animateToState("StandHowl");
+    });
+  }
+}
+
+for (var i = 0; i < size; i++) {
+  for (var j = 0; j < size; j++) {
+    var id = DX.createItem("CheckerChip", i * 2 - 6, j * 2 - 6, -0.2);
+    var chip = DX.item(id);
+    chip.setScale(2);
+    chip.setColor(0, 128, 0);
+    chip.activate(jump(i * 2 - 6, j * 2 - 6));
+  }
+}
