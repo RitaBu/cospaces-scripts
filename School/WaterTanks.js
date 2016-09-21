4f6aa891461da4169a55a89d33eeb3861d847b5a
beg <beg@delightex.com>
2016-09-21 22:15:40 +0200

    Renamed variables

--- School/WaterTanks.js
+++ School/WaterTanks.js
@@ -1,17 +1,17 @@
 // http://newcospaces.dx.labs.intellij.net/shorten/nNyw
 // https://cospac.es/YKGQ
 
-var f10 = DX.item("tank1");
-var f1 = DX.item("water1");
-var f2 = DX.item("water2");
+var tank1 = DX.item("tank1");
+var water1 = DX.item("water1");
+var water2 = DX.item("water2");
 var pipe = DX.item("pipe");
 var w = DX.item("weight");
-var h = f10.getHeight();
+var h = tank1.getHeight();
 var weight = true;
 
 pipe.setColor(0, 100, 255);
-f1.setNumVertices(64);
-f2.setNumVertices(64);
+water1.setNumVertices(64);
+water2.setNumVertices(64);
 
 resetWithWeight();
 
@@ -20,30 +20,31 @@ var exec = false;
 DX.setHeartbeatInterval(0.01);
 DX.heartbeat(function(dt) {
   var step = 0.02;
-  var h1 = f1.getHeight();
-  var h2 = f2.getHeight();
-  if(weight){
-    if(h1 > 0){
-      f1.setHeight(h1 - step);
-      f2.setHeight(h2 + step);
-      pos = f1.position();
-      w.setPosition(pos[0], pos[1], pos[2] + f1.getHeight());
+  var h1 = water1.getHeight();
+  var h2 = water2.getHeight();
+  var pos;
+  if (weight) {
+    if (h1 > 0) {
+      water1.setHeight(h1 - step);
+      water2.setHeight(h2 + step);
+      pos = water1.position();
+      w.setPosition(pos[0], pos[1], pos[2] + water1.getHeight());
     }
-    else{
-      pipe.setColor(255,255,255);
-      if(!exec){
+    else {
+      pipe.setColor(255, 255, 255);
+      if (!exec) {
         exec = true;
         DX.runLater(resetWithoutWeight, 1);
       }
     }
   }
-  else{
-    if(h2 > h1){
-      f1.setHeight(h1 + step);
-      f2.setHeight(h2 - step);
+  else {
+    if (h2 > h1) {
+      water1.setHeight(h1 + step);
+      water2.setHeight(h2 - step);
     }
-    else{
-      if(!exec){
+    else {
+      if (!exec) {
         exec = true;
         DX.runLater(resetWithWeight, 1);
       }
@@ -53,10 +54,10 @@ DX.heartbeat(function(dt) {
 });
 
 function resetWithWeight(){
-  f1.setHeight(h * 0.5 - 0.1);
-  f2.setHeight(h * 0.5 - 0.1);
-  pos = f1.position();
-  w.setPosition(pos[0], pos[1], pos[2] + f1.getHeight());
+  water1.setHeight(h * 0.5 - 0.1);
+  water2.setHeight(h * 0.5 - 0.1);
+  var pos = water1.position();
+  w.setPosition(pos[0], pos[1], pos[2] + water1.getHeight());
   weight = true;
   exec = false;
 }
