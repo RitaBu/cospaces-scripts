52caa47d3a128d4c7f730f2b7c99880b69395efc
beg <beg@delightex.com>
2016-09-28 00:06:14 +0200

    Working

--- Samples/EyeSample1.js
+++ Samples/EyeSample1.js
@@ -1,6 +1,6 @@
 var eye_js = "https://raw.githubusercontent.com/delightex/cospaces-scripts/master/Samples/Eye.js";
 
-DX.loadScript(formation_js, function () {
+DX.loadScript(eye_js, function () {
   var eye = DX.item("naihDdoqEK");
   DX.runLater(blink(eye));
 });
