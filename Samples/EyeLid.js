e31b125f505ac3a7de78c7f50018b913a42e6251
beg <beg@delightex.com>
2016-09-30 21:10:07 +0200

    Working

--- Samples/EyeLid.js
+++ Samples/EyeLid.js
@@ -4,91 +4,110 @@ const STATE_CLOSED = "CLOSED";
 var startTime = 0;
 var totalTime = 0;
 
-var Animation = function(duration, callback) {
+var Animation = function (name, duration, exec) {
+  this.name = name;
   this.duration = duration;
   this.finished = true;
-  this.callback = callback;
+  this.exec = exec;
 };
 
-Animation.prototype.start = function(t) {
+Animation.prototype.toString = function () {
+  return "[Animation] " + this.name;
+}
+
+Animation.prototype.start = function (t) {
   this.startTime = t;
+  this.currentTime = 0;
   this.finished = false;
 };
 
 Animation.prototype.update = function (t) {
-  if (t - this.startTime < this.duration) {
+  if ((t - this.startTime) > this.duration) {
     this.finished = true;
-    this.callback();
+    DX.log(this.toString() + " finished");
+    return;
   }
+  this.currentTime = t;
+  this.exec(this.getProgress());
 };
 
-var EyeLid = function(item, state) {
+Animation.prototype.getProgress = function () {
+  var timeLeft = this.startTime + this.duration - this.currentTime;
+  var p = 1 - timeLeft / this.duration;
+  DX.log("Timeleft " + timeLeft);
+  DX.log("duration " + this.duration);
+  return p;
+};
+
+var EyeLid = function (item, state) {
   this.item = item;
   this.state = state;
 };
 
+/*
 EyeLid.prototype.isAnimated = function () {
   if (this.animation === undefined) return false;
   return !this.animation.finished;
 };
+*/
 
 EyeLid.prototype.update = function (t) {
-  this.animation.update(t);
-  if (!this.isAnimated()) return;
-
+  if (this.animation !== undefined && this.animation !== null) {
+    if (!this.animation.finished) {
+      DX.log("Progress: " + this.animation.getProgress());
+      this.animation.update(t);
+    }
+  }
 };
 
 EyeLid.prototype.close = function () {
-  this.close(function () {});
-};
-
-EyeLid.prototype.close = function (callback) {
   if (this.state === STATE_CLOSED) return;
   this.state = STATE_CLOSED;
-  this.animation = new Animation(0.5, callback);
+  var that = this;
+  this.animation = new Animation("Close", 1, function (progress) {
+    DX.log("Here");
+    that.item.rotateLocalAxis(0, 1, 0, 0, 1, 0, 45 * progress, true);
+  });
+  DX.log("Starting closing animation");
   this.animation.start(totalTime);
 };
 
 EyeLid.prototype.open = function () {
-  this.open(function () {});
-};
-
-EyeLid.prototype.open = function (callback) {
   if (this.state === STATE_OPEN) return;
   this.state = STATE_OPEN;
-  this.animation = new Animation(0.5, callback);
+  var that = this;
+  this.animation = new Animation("Open", 1, function (progress) {
+    that.item.rotateLocalAxis(0, 0, 0, 0, 1, 0, -45 * progress, true);
+  });
   this.animation.start(totalTime);
 };
 
 EyeLid.prototype.blink = function () {
-  var that = this;
-  this.close(function () {
-    that.open();
-  });
+  this.close();
 };
 
-var eyeLid = new EyeLid(DX.item("itwDLHAkPv"), STATE_OPEN);
+var eyeLid = new EyeLid(DX.item("7mxj4NOSqq"), STATE_OPEN);
 
-DX.setHeartbeatInterval(1);
+DX.setHeartbeatInterval(0.1);
 DX.heartbeat(function (dt) {
   if (startTime === 0) {
     startTime = dt;
   }
   totalTime = dt - startTime;
-  DX.log("Total time: " + totalTime);
   //x, y, z, axisX, axisY, axisZ, angle, discrete
   //eyeLid.rotateLocalAxis(0, 0, 0, 0, 1, 0, -dt * 0.0001, true);
   //eyeLid.moveLocal(1, 0, 0);
-  if (eyeLid.isAnimated()) {
-    eyeLid.update(totalTime);
-  }
+  DX.log("Total time: " + totalTime);
+  eyeLid.update(totalTime);
 });
 
 function blinkRepeat(eyeLid) {
   eyeLid.blink();
-  DX.runLater(function () {
-    blinkRepeat(eyeLid);
-  }, 2);
+  /*
+   DX.runLater(function () {
+   blinkRepeat(eyeLid);
+   }, 2);
+   */
 }
 
 blinkRepeat(eyeLid);
