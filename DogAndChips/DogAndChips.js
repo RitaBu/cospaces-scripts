e0251b871720d4a2ba9313f53d49f8f133042c7d
Pavel Kakolin <pavel.kakolin@jetbrains.com>
2016-09-30 14:04:08 +0200

    fix Dog & Chips game for new script engine

--- DogAndChips/DogAndChips.js
+++ DogAndChips/DogAndChips.js
@@ -1,12 +1,11 @@
 var size = 10;
 
-var items = DX.items();
+var items = Space.items();
 for (var k = 0; k < items.length; k++) {
   items[k].remove();
 }
 
-var dogId = DX.createItem("LP_Dog", 0, 0, 1);
-var dog = DX.item(dogId);
+var dog = Space.createItem("LP_Dog", 0, 0, 1);
 dog.setColor(162, 42, 42);
 
 function jump(x, y) {
@@ -20,8 +19,7 @@ function jump(x, y) {
 
 for (var i = 0; i < size; i++) {
   for (var j = 0; j < size; j++) {
-    var id = DX.createItem("CheckerChip", i * 2 - size + 1, j * 2 - size + 1, -0.2);
-    var chip = DX.item(id);
+    var chip = Space.createItem("CheckerChip", i * 2 - size + 1, j * 2 - size + 1, -0.2);
     chip.setScale(2);
     chip.setColor(0, 128, 0);
     chip.activate(jump(i * 2 - size + 1, j * 2 - size + 1));
