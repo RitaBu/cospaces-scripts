37641c3e0449436d27848f554ab640c832b6d793
beg <beg@delightex.com>
2016-10-03 12:22:50 +0200

    Eye blinking animation

--- Samples/EyeLid.js
+++ Samples/EyeLid.js
@@ -1,6 +1,6 @@
 const STATE_OPEN = "OPEN";
 const STATE_CLOSED = "CLOSED";
-const BLINK_DURATION = 0.15;
+const BLINK_DURATION = 0.11;
 
 var startTime = 0;
 var totalTime = 0;
@@ -49,24 +49,17 @@ Animation.prototype.getProgress = function () {
   return 1 - timeLeft / this.duration;
 };
 
-var EyeLid = function (item, state) {
+var Eyelid = function (item, state) {
   this.item = item;
   this.state = state;
   this.anims = [];
 };
 
-/*
- EyeLid.prototype.isAnimated = function () {
- if (this.animation === undefined) return false;
- return !this.animation.finished;
- };
- */
-
-EyeLid.prototype.update = function (t) {
+Eyelid.prototype.update = function (t) {
   if (this.anims.length > 0) {
     var a = this.anims[0];
     a.update(t);
-    DX.log(a.toString() + " Progress: " + a.getProgress());
+    // DX.log(a.toString() + " Progress: " + a.getProgress());
     if (a.finished) {
       this.anims.shift();
       if (this.anims.length > 0) {
@@ -78,14 +71,14 @@ EyeLid.prototype.update = function (t) {
   }
 };
 
-EyeLid.prototype.addAnimation = function (a) {
+Eyelid.prototype.addAnimation = function (a) {
   this.anims.push(a);
   if (this.anims.length == 1) {
     a.start(totalTime);
   }
-}
+};
 
-EyeLid.prototype.down = function () {
+Eyelid.prototype.down = function () {
   /*
    if (this.state === STATE_CLOSED) return;
    this.state = STATE_CLOSED;
@@ -96,13 +89,13 @@ EyeLid.prototype.down = function () {
     return function (anim) {
       var p = lastProgress;
       lastProgress = 75 * anim.getProgress();
-      DX.log(anim.toString() + " lastProgress = " + lastProgress);
+      // DX.log(anim.toString() + " lastProgress = " + lastProgress);
       that.item.rotateLocalAxis(0, 1, 0, 0, 1, 0, Math.radians(-(lastProgress - p)), true);
     };
   })()));
 };
 
-EyeLid.prototype.up = function () {
+Eyelid.prototype.up = function () {
   /*
    if (this.state === STATE_OPEN) return;
    this.state = STATE_OPEN;
@@ -113,24 +106,43 @@ EyeLid.prototype.up = function () {
     return function (anim) {
       var p = lastProgress;
       lastProgress = 75 * anim.getProgress();
-      DX.log(anim.toString() + " lastProgress = " + lastProgress);
+      // DX.log(anim.toString() + " lastProgress = " + lastProgress);
       that.item.rotateLocalAxis(0, 1, 0, 0, 1, 0, Math.radians(lastProgress - p), true);
     };
   })()));
 };
 
-EyeLid.prototype.blinkTop = function () {
+Eyelid.prototype.blinkTop = function () {
   this.down();
   this.up();
 };
 
-EyeLid.prototype.blinkBottom = function () {
+Eyelid.prototype.blinkBottom = function () {
   this.up();
   this.down();
 };
 
-var topEyeLid = new EyeLid(DX.item("oRE07xiEX3"), STATE_OPEN);
-var bottomEyeLid = new EyeLid(DX.item("Aytf70hzTt"), STATE_OPEN);
+var Eye = function (eyeItem) {
+  this.topEyelid = new Eyelid(eyeItem.part("topEyelid"), STATE_OPEN);
+  this.bottomEyelid = new Eyelid(eyeItem.part("bottomEyelid"), STATE_OPEN);
+};
+
+Eye.prototype.update = function (t) {
+  this.topEyelid.update(t);
+  this.bottomEyelid.update(t);
+};
+
+Eye.prototype.blink = function () {
+  this.topEyelid.blinkTop();
+  this.bottomEyelid.blinkBottom();
+};
+
+var topEyelid = new Eyelid(DX.item("oRE07xiEX3"), STATE_OPEN);
+var bottomEyelid = new Eyelid(DX.item("Aytf70hzTt"), STATE_OPEN);
+
+var rightEye = new Eye(DX.item("HsahejpO9w"));
+var leftEye = new Eye(DX.item("roDyL5azf3"));
+
 
 DX.setHeartbeatInterval(0);
 DX.heartbeat(function (dt) {
@@ -139,26 +151,19 @@ DX.heartbeat(function (dt) {
   }
   totalTime = dt - startTime;
   //x, y, z, axisX, axisY, axisZ, angle, discrete
-  //topEyeLid.rotateLocalAxis(0, 0, 0, 0, 1, 0, -dt * 0.0001, true);
-  //topEyeLid.moveLocal(1, 0, 0);
+  //topEyelid.rotateLocalAxis(0, 0, 0, 0, 1, 0, -dt * 0.0001, true);
+  //topEyelid.moveLocal(1, 0, 0);
   // DX.log("Total time: " + totalTime);
-  topEyeLid.update(totalTime);
-  bottomEyeLid.update(totalTime);
+  leftEye.update(totalTime);
+  rightEye.update(totalTime);
 });
 
-function blinkRepeatTop(eyeLid) {
-  eyeLid.blinkTop();
+function blinkRepeatEye(eye) {
+  eye.blink();
    DX.runLater(function () {
-   blinkRepeatTop(eyeLid);
+     blinkRepeatEye(eye);
    }, 2);
 }
 
-function blinkRepeatBottom(eyeLid) {
-  eyeLid.blinkBottom();
-   DX.runLater(function () {
-   blinkRepeatBottom(eyeLid);
-   }, 2);
-}
-
-blinkRepeatTop(topEyeLid);
-blinkRepeatBottom(bottomEyeLid);
+blinkRepeatEye(leftEye);
+blinkRepeatEye(rightEye);
