f4848718d75e89ffe82d4c313f1c03b794dfeb0c
beg <beg@delightex.com>
2016-10-06 19:29:13 +0200

    Code review

--- City/DynamicCity.js
+++ City/DynamicCity.js
@@ -51,6 +51,21 @@ function createPathFromPoints(points) {
   return vec;
 }
 
+/*
+City.addCarsToPath(vec, n, function (i) {
+  var car;
+  if (n < 5) {
+    car = Space.createItem("LP_Bus", 0, 0, 0);
+    car.setScale(0.5);
+    car.setColor(0, 255, 0);
+  } else {
+    car = Space.createItem("LP_Car", 0, 0, 0);
+    car.setColor(255, 255, 0);
+  }
+  return car;
+});
+*/
+
 function addCarsToPath(vec, n, color) {
   var nl = vec.length;
   var car;
@@ -61,8 +76,10 @@ function addCarsToPath(vec, n, color) {
     }
     var modelId = "LP_Car";
     car = Space.createItem(modelId, vec[i].position().x, vec[i].position().y, 0);
-    if (color != null)
+    if (color != null) {
       car.setColor(color[0], color[1], color[2]);
+    }
+
     car.addToBezier3DPathCurve(line.id());
     car.finishBezier3DPath(2, true);
     car.setScale(carScale);
@@ -110,12 +127,7 @@ function createPath4() {
   addCarsToPath(vec, 8);
 }
 
-createPath1();
-createPath2();
-createPath3();
-createPath4();
-
-var file = "%%98efa9b173c24d877a7d54f51889bc5a7d98d0b9b747c4e771cf53589e1b41fc:"
+var trafficLightModelId = "%%98efa9b173c24d877a7d54f51889bc5a7d98d0b9b747c4e771cf53589e1b41fc:";
 var states = ["D", "C", "A", "B"];
 
 function addTrafficLight(h, w, dt) {
@@ -124,19 +136,19 @@ function addTrafficLight(h, w, dt) {
   var y = pos.y;
   var d = 0.6 * bs;
   Space.log(x + " " + y);
-  var item0 = Space.createItem(file + states[0], x + d, y + d, 0);
+  var item0 = Space.createItem(trafficLightModelId + states[0], x + d, y + d, 0);
   item0.setRotationOZ(1, 0, true);
   item0.setProperty("light", "red");
 
-  var item1 = Space.createItem(file + states[0], x - d, y - d, 0);
+  var item1 = Space.createItem(trafficLightModelId + states[0], x - d, y - d, 0);
   item1.setRotationOZ(-1, 0, true);
   item1.setProperty("light", "red");
 
-  var item2 = Space.createItem(file + states[0], x - d, y + d, 0);
+  var item2 = Space.createItem(trafficLightModelId + states[0], x - d, y + d, 0);
   item2.setRotationOZ(0, 1, true);
   item2.setProperty("light", "red");
 
-  var item3 = Space.createItem(file + states[0], x + d, y - d, 0);
+  var item3 = Space.createItem(trafficLightModelId + states[0], x + d, y - d, 0);
   item3.setRotationOZ(0, -1, true);
   item3.setProperty("light", "red");
 
@@ -148,36 +160,36 @@ function addTrafficLight(h, w, dt) {
 
     if (index === 0) {
       item0.setProperty("light", "red");
-      item0.setModelId(file + states[0]);
+      item0.setModelId(trafficLightModelId + states[0]);
       item1.setProperty("light", "red");
-      item1.setModelId(file + states[0]);
+      item1.setModelId(trafficLightModelId + states[0]);
 
       item2.setProperty("light", "green");
-      item2.setModelId(file + states[2]);
+      item2.setModelId(trafficLightModelId + states[2]);
       item3.setProperty("light", "green");
-      item3.setModelId(file + states[2]);
+      item3.setModelId(trafficLightModelId + states[2]);
       pause = 6;
     } else if (index === 2) {
       item0.setProperty("light", "green");
-      item0.setModelId(file + states[2]);
+      item0.setModelId(trafficLightModelId + states[2]);
       item1.setProperty("light", "green");
-      item1.setModelId(file + states[2]);
+      item1.setModelId(trafficLightModelId + states[2]);
 
       item2.setProperty("light", "red");
-      item2.setModelId(file + states[0]);
+      item2.setModelId(trafficLightModelId + states[0]);
       item3.setProperty("light", "red");
-      item3.setModelId(file + states[0]);
+      item3.setModelId(trafficLightModelId + states[0]);
       pause = 6;
     } else {
       item0.setProperty("light", "yellow");
-      item0.setModelId(file + states[3]);
+      item0.setModelId(trafficLightModelId + states[3]);
       item1.setProperty("light", "yellow");
-      item1.setModelId(file + states[3]);
+      item1.setModelId(trafficLightModelId + states[3]);
 
       item2.setProperty("light", "yellow");
-      item2.setModelId(file + states[3]);
+      item2.setModelId(trafficLightModelId + states[3]);
       item3.setProperty("light", "yellow");
-      item3.setModelId(file + states[3]);
+      item3.setModelId(trafficLightModelId + states[3]);
       pause = 3;
     }
     Space.schedule(tick, pause);
@@ -185,6 +197,11 @@ function addTrafficLight(h, w, dt) {
 
   Space.schedule(tick, 1 + dt);
 }
+
+createPath1();
+createPath2();
+createPath3();
+createPath4();
 
 addTrafficLight(1, 1, 0);
 addTrafficLight(1, 2, 1);
