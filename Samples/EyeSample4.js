1176e1ec838fa05a3a539edb2c24d3cdab5a8ebd
beg <beg@delightex.com>
2016-10-11 21:52:38 +0200

    Eyed object

--- Samples/EyeSample4.js
+++ Samples/EyeSample4.js
@@ -1,32 +1,5 @@
 Space.loadLibrary("https://raw.githubusercontent.com/delightex/cospaces-scripts/master/Samples/", function () {
-  require(['AnimatedEye'], function (AnimatedEye) {
-    var EyedObject = function (item) {
-      this.leftEye = new AnimatedEye(item.part("LeftEye"));
-      this.rightEye = new AnimatedEye(item.part("RightEye"));
-    };
-
-    EyedObject.prototype.blink = function () {
-      this.leftEye.blink();
-      this.rightEye.blink();
-    };
-
-    EyedObject.prototype.right = function () {
-      this.leftEye.right();
-      this.rightEye.right();
-    };
-
-    EyedObject.prototype.left = function () {
-      this.leftEye.left();
-      this.rightEye.left();
-    };
-
-    EyedObject.prototype.update = function () {
-      this.leftEye.update();
-      this.rightEye.update();
-    };
-
-// ===================================================================
-
+  require(['EyedObject'], function (EyedObject) {
     var elephant = new EyedObject(Space.item("imapObWTsT"));
     var tree = new EyedObject(Space.item("Jsqj60uMeS"));
     var tree2 = new EyedObject(Space.item("4tvtLJYadq"));
---
+++ Samples/EyedObject.js
@@ -0,0 +1,29 @@
+define(['AnimatedEye'], function (AnimatedEye) {
+  var EyedObject = function (item) {
+    this.leftEye = new AnimatedEye(item.part("LeftEye"));
+    this.rightEye = new AnimatedEye(item.part("RightEye"));
+  };
+
+  EyedObject.prototype.blink = function () {
+    this.leftEye.blink();
+    this.rightEye.blink();
+  };
+
+  EyedObject.prototype.right = function () {
+    this.leftEye.right();
+    this.rightEye.right();
+  };
+
+  EyedObject.prototype.left = function () {
+    this.leftEye.left();
+    this.rightEye.left();
+  };
+
+  EyedObject.prototype.update = function () {
+    this.leftEye.update();
+    this.rightEye.update();
+  };
+
+  return EyedObject;
+});
+
