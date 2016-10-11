c8f4b8c8266c071e9324a7fde476e4c4dbddbd65
beg <beg@delightex.com>
2016-10-11 21:46:38 +0200

    Fly sample

---
+++ Samples/Fly.js
@@ -0,0 +1,53 @@
+define(['../helpers/animation/animation'], function (animation) {
+  var Fly = function (item) {
+    this.animator = new animation.Animator(false);
+    this.item = item;
+    this.rightWing = item.part("RightWing");
+    this.leftWing = item.part("LeftWing");
+  };
+
+  Fly.prototype.update = function () {
+    this.animator.update();
+  };
+
+  Fly.prototype.wingsUp = function () {
+    var that = this;
+    var a = new animation.Animation("Wings Up", 0.02, (function () {
+      var lastProgress = 0;
+      const angle = 45;
+      return function (anim) {
+        var p = lastProgress;
+        lastProgress = angle * anim.getProgress();
+        // Space.log(anim.toString() + " lastProgress = " + lastProgress);
+        that.rightWing.rotateLocalAxis(1, 0, 0, 1, 0, 0, Math.radians(-(lastProgress - p)), true);
+        that.leftWing.rotateLocalAxis(1, 0, 0, 1, 0, 0, -Math.radians(-(lastProgress - p)), true);
+      };
+    })(), false);
+    this.animator.addAnimation(a);
+  };
+
+  Fly.prototype.wingsDown = function () {
+    var that = this;
+    var a = new animation.Animation("Wings Down", 0.02, (function () {
+      var lastProgress = 0;
+      const angle = 45;
+      return function (anim) {
+        var p = lastProgress;
+        lastProgress = angle * anim.getProgress();
+        // Space.log(anim.toString() + " lastProgress = " + lastProgress);
+        that.rightWing.rotateLocalAxis(1, 0, 0, 1, 0, 0, -Math.radians(-(lastProgress - p)), true);
+        that.leftWing.rotateLocalAxis(1, 0, 0, 1, 0, 0, Math.radians(-(lastProgress - p)), true);
+      };
+    })(), false);
+    this.animator.addAnimation(a);
+  };
+
+  Fly.prototype.swing = function () {
+    this.wingsUp();
+    this.wingsDown();
+    // this.wingsUp();
+    // this.wingsDown();
+  };
+
+  return Fly;
+});
---
+++ Samples/FlySample.js
@@ -0,0 +1,19 @@
+Space.loadLibrary("https://raw.githubusercontent.com/delightex/cospaces-scripts/master/helpers/Samples/", function () {
+  require(['Fly'], function (Fly) {
+
+    var fly = new Fly(Space.item("q5VWCw7hVA"));
+
+    Space.scheduleRepeating(function () {
+      fly.update();
+    }, 0);
+
+    function flySwing() {
+      fly.swing();
+      Space.schedule(function () {
+        flySwing();
+      }, 0.1);
+    }
+
+    flySwing();
+  });
+});
