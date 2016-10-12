f0d95c9259843b527481f472546adefd3df74081
beg <beg@delightex.com>
2016-10-12 13:52:17 +0200

    MakerFaire demo initial commit

---
+++ MakerFaireDemo/AnimatedTongue.js
@@ -0,0 +1,24 @@
+define(['../helpers/animation/animation'], function (animation) {
+  var AnimatedTongue = function (item) {
+    this.animator = new animation.Animator(false);
+  };
+
+  AnimatedTongue.prototype.update = function() {
+    this.animator.update();
+  };
+
+  AnimatedTongue.prototype.out = function () {
+    this.animator.addAnimation();
+  };
+
+  AnimatedTongue.prototype.in = function () {
+    this.animator.addAnimation();
+  };
+
+  AnimatedTongue.prototype.toggle = function () {
+    this.out();
+    this.in();
+  };
+
+  return AnimatedTongue;
+});
