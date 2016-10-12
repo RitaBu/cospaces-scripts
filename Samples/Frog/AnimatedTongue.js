6c1708d344b1b44ceb1e83f6bc74ef49f38f6b5a
Ruslan Zabrodin <ruslan@delightex.com>
2016-10-12 15:04:51 +0200

    change animated tongue

--- Samples/Frog/AnimatedTongue.js
+++ Samples/Frog/AnimatedTongue.js
@@ -1,18 +1,36 @@
 define(['../../helpers/animation/animation'], function (animation) {
+  var DURATION = 0.5;
+
   var AnimatedTongue = function (item) {
     this.animator = new animation.Animator(false);
   };
 
-  AnimatedTongue.prototype.update = function() {
+  AnimatedTongue.prototype.update = function () {
     this.animator.update();
   };
 
   AnimatedTongue.prototype.out = function () {
-    this.animator.addAnimation();
+    var that = this;
+    this.animator.addAnimation(new animation.Animation("Out", DURATION, (function () {
+      var sizeZ = that.item.getSize()[2];
+      const range = 0.5;
+      return function (anim) {
+        var p = anim.getProgress();
+        that.item.setZ(sizeZ + range * p);
+      };
+    })()));
   };
 
   AnimatedTongue.prototype.in = function () {
-    this.animator.addAnimation();
+    var that = this;
+    this.animator.addAnimation(new animation.Animation("In", DURATION, (function () {
+      const range = 0.5;
+      var sizeZ = that.item.getSize()[2];
+      return function (anim) {
+        var p = anim.getProgress();
+        that.item.setZ(sizeZ - range * p);
+      };
+    })()));
   };
 
   AnimatedTongue.prototype.toggle = function () {
