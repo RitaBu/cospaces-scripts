8afb9867db1be00fe8fce2413f784ffb4ef513a6
Pavel Kakolin <pavel.kakolin@jetbrains.com>
2016-10-12 18:06:16 +0200

    testing chaching fix

--- Samples/Frog/AnimatedTongue.js
+++ Samples/Frog/AnimatedTongue.js
@@ -1,5 +1,5 @@
 define(['../../helpers/animation/animation'], function (animation) {
-  var DURATION = 0.2; //test
+  var DURATION = 0.2;
 
   var AnimatedTongue = function (item) {
     this.item = item;
