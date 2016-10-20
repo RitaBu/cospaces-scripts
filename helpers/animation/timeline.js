8c55d3869cb1fb256a078239c8de01cc2102313c
beg <beg@delightex.com>
2016-10-21 00:22:02 +0200

    Timeline animation

--- helpers/animation/timeline.js
+++ helpers/animation/timeline.js
@@ -1,9 +1,9 @@
 define(function () {
-  var Timeline = function (duration) {
+  var Timeline = function (duration, updateInterval) {
     this.duration = duration;
     this.startTime = 0;
     this.scheduled = null;
-    this.updateInterval = 1;
+    this.updateInterval = updateInterval;
     this.lastUpdateTime = 0;
     this.allTimes = [];
     this.allAnims = [];
@@ -11,6 +11,9 @@ define(function () {
   };
 
   Timeline.prototype.addAnimation = function (a, time) {
+    if (this.startTime > 0 && ((Space.currentTime() - this.lastUpdateTime) > time)) {
+      Project.log("Time elapsed even before starting for " + a);
+    }
     this.allTimes.push(time);
     this.allAnims.push(a);
   };
@@ -19,26 +22,34 @@ define(function () {
     var that = this;
     this.startTime = Space.currentTime();
     this.lastUpdateTime = this.startTime;
-    this.scheduled = Space.schedule(function () {
-      if ((Space.currentTime() - that.lastUpdateTime) >= this.updateInterval) {
-        that.lastUpdateTime = Space.currentTime();
-        that.allTimes.forEach(function (t, index) {
-          if (t >= (Space.currentTime() - that.lastUpdateTime)) {
-            var a = that.allAnims[index];
-            that.runningAnims.push(a);
-            that.allAnims.remove(index);
-            that.allTimes.remove(index);
-            a.start();
-          }
-        });
-        that.runningAnims.forEach(function (a, index) {
-          a.update();
-          if (a.finished) {
-            that.runningAnims.splice(index, 1);
-          }
-        });
-      }
+    this.scheduled = Space.scheduleRepeating(function () {
+      that.update();
     }, 0);
+  };
+
+  Timeline.prototype.update = function () {
+    var that = this;
+    var timePassed = Space.currentTime() - that.lastUpdateTime;
+    if ((timePassed) >= this.updateInterval) {
+      that.lastUpdateTime = Space.currentTime();
+      that.allTimes.forEach(function (t, index) {
+        var totalTimePassed = Space.currentTime() - that.startTime;
+        if (t >= (totalTimePassed)) {
+          //Project.log("Total time passed: " + totalTimePassed);
+          var a = that.allAnims[index];
+          that.runningAnims.push(a);
+          that.allAnims.splice(index, 1);
+          that.allTimes.splice(index, 1);
+          a.start();
+        }
+      });
+      that.runningAnims.forEach(function (a, index) {
+        a.update();
+        if (a.finished) {
+          that.runningAnims.splice(index, 1);
+        }
+      });
+    }
   };
 
   return Timeline;
