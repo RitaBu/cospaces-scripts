f5d257489070bd85ae25429d77e36a0be1461f68
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-10-21 21:42:08 +0200

    methods for animating butterfly renamed

--- Samples/Butterflies.js
+++ Samples/Butterflies.js
@@ -1,6 +1,12 @@
-//http://newcospaces.dx.labs.intellij.net/#Project:CkTcwhNsVW0.GWs1ktTIKDY:B89ljVSJD95.HE9vtcbV1x4
+//https://newcospaces.dx.labs.intellij.net/#Project:CkTcwhNsVW0.GWs1ktTIKDY:B89ljVSJD95.HE9vtcbV1x4
 
-var items = Space.getItems();
+var butterfly = Space.getItem("yAv6S4NU8p");
+var items = [butterfly];
+
+var tree = Space.getItem("BGkOM-4zZgw.C6EO0xxP9K3");
+var trees = [tree];
+
+
 var bool = [false, false, false, false, false, false, false, false];
 var free = [true, true, true, true, true, true, true, true];
 var treeIdxs = [-1, -1, -1, -1, -1, -1, -1, -1];
@@ -9,24 +15,32 @@ function fly(index, item) {
   return function() {
     bool[index] = !bool[index];
     if (bool[index]) {
-      item.flyLikeButterfly(0, 0, 3);
+      item.moveBezierCircle(0, 0, 3);
+      item.playIdleAnimation("Fly");
+
       free[treeIdxs[index]] = true;
+      Space.schedule(fly(index, items[index]), Math.random() * 40);
     } else {
-      var treeIdx;
-      do {
-        treeIdx = Math.floor(Math.random() * 8);
-      } while (!free[treeIdx]);
+      var treeIdx = 0;
+      /*var treeIdx;
+       do {
+       treeIdx = Math.floor(Math.random() * 8);
+       } while (!free[treeIdx]);*/
 
       free[treeIdx] = false;
       treeIdxs[index] = treeIdx;
 
-      var tree = items[treeIdx + 8];
-      item.flyLikeButterflyToObj(tree, "Top");
+      var stop = function() {
+        items[index].animateToState("Stand");
+        Space.schedule(fly(index, items[index]), 5);
+      };
+
+      var tree = trees[treeIdx];
+      item.moveBezierToObj(tree, "Top", stop);
     }
-    Space.schedule(fly(index, items[index]), Math.random() * 40);
   };
 }
 
-for (i = 0; i < 8; i++) {
-  Space.schedule(fly(i, items[i]), Math.random() * 10);
+for (i = 0; i < 1; i++) {
+  Space.schedule(fly(i, items[i]), Math.random() * 5);
 }
