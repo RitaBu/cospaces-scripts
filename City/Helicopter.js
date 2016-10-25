60bee12ac2d3e52f33957ea42b40d9b91ed13c96
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-10-25 20:37:30 +0200

    code formatting

--- City/Helicopter.js
+++ City/Helicopter.js
@@ -7,19 +7,19 @@ var zFlight = 20;
 var radius = 12;
 var velocity = 3;
 
-var flightHeli = function(){
+var flightHeli = function() {
 
   heli.startHelicopter();
-  Space.schedule(function(){
+  Space.schedule(function() {
     heli.moveBezierToWithCallback(start.x, start.y, zFlight, velocity, function(){
       heli.moveBezierCircle(0, 0, zFlight, radius, velocity);
-      Space.schedule(function(){
+      Space.schedule(function() {
         heli.moveBezierToObj(building, "Top", velocity, stopHeli);
       }, 30);
     });
   }, 5);
 
-  var stopHeli = function(){
+  var stopHeli = function() {
     heli.stopHelicopter();
     Space.schedule(flightHeli, 10);
   }
