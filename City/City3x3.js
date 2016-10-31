f5b2ef8307bfc207af855fb6355b3cc2e4bd03c9
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-10-31 17:45:41 +0100

    build script for city 3x3

---
+++ City/City3x3.js
@@ -0,0 +1,54 @@
+var city = Space.createCity(0, 0);
+city.buildGridCity(3, 3, 1);
+
+city.addTrees(1, 0);
+city.addTrees(1, 2);
+
+city.addTrees(3, 6);
+
+city.addTrees(4, 1);
+city.addTrees(4, 5);
+
+city.addTrees(5, 0);
+city.addTrees(5, 6);
+
+city.addTrees(6, 1);
+city.addTrees(6, 3);
+city.addTrees(6, 5);
+
+city.addLamps(1, 2);
+city.addLamps(1, 4);
+
+city.addLamps(2, 1);
+city.addLamps(2, 3);
+
+city.addLamps(3, 2);
+city.addLamps(3, 4);
+
+city.addLamps(4, 1);
+city.addLamps(4, 3);
+city.addLamps(4, 5);
+
+city.addLamps(5, 2);
+city.addLamps(5, 4);
+
+city.addSigns(1, 2);
+city.addSigns(1, 4);
+
+city.addSigns(2, 1);
+city.addSigns(2, 3);
+
+city.addSigns(3, 2);
+city.addSigns(3, 4);
+
+city.addSigns(4, 1);
+city.addSigns(4, 3);
+city.addSigns(4, 5);
+
+city.addSigns(5, 2);
+city.addSigns(5, 4);
+
+city.rebuild();
+
+Space.renderShadows(false);
+
