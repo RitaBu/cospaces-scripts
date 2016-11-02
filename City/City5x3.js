64ca099e0c15c8bda96bdba5d381727fec5ebeeb
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-11-02 12:30:00 +0100

    script for city

--- City/City5x3.js
+++ City/City5x3.js
@@ -16,9 +16,6 @@ city.addTrees(6, 1);
 city.addTrees(6, 3);
 city.addTrees(6, 5);
 
-city.addTrees(8, 5);
-city.addTrees(9, 4);
-
 city.addLamps(1, 2);
 city.addLamps(1, 4);
 
@@ -53,8 +50,34 @@ city.addSigns(5, 4);
 
 city.rebuild();
 
-city.createTrajectory([2, 0, 2, 2, 4, 2, 4, 0]);
-
 Space.renderShadows(false);
 Space.renderServiceItems(true);
 
+var rs = city.roadWidth() / 4;
+var bs = city.blockSize() / 2;
+
+var velocity = 2;
+
+function createPath(path) {
+    var lineId = city.createTrajectory(path);
+    var line = Space.getItem(lineId);
+
+    var n = path.length / 2;
+    var i = 0;
+
+    var modelId = i % 2 === 0 ? "LP_Car" : "LP_Bus";
+    var car = Space.createItem(modelId);
+    car.moveBezier([line.id()], velocity, true);
+
+    for (i = 1; i < n; i++) {
+        line = line.shift();
+
+        modelId = i % 2 === 0 ? "LP_Car" : "LP_Bus";
+        car = Space.createItem(modelId);
+        car.moveBezier([line.id()], velocity, true);
+    }
+}
+
+createPath([0, 0, 0, 2, 2, 2, 2, 0]);
+
+Space.setCarDriveController(3, 1.5);
