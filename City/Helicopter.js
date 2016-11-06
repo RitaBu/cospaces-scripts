f49be6b7a2484597ec82484bada600a25b65e15b
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-11-06 14:30:21 +0100

    heli script

--- City/Helicopter.js
+++ City/Helicopter.js
@@ -1,7 +1,6 @@
 var heli = Space.createItem("LP_Helicopter");
 var building = Space.getItem("sDAMBMkTCaaSwhXczOyOdDP");
 var start = building.getPosition();
-heli.setPosition(start.x, start.y, start.z + 7);
 
 var zFlight = 20;
 var radius = 12;
@@ -11,7 +10,7 @@ var flightHeli = function() {
 
   heli.startHelicopter();
   Space.schedule(function() {
-    heli.moveBezierToWithCallback(start.x, start.y, zFlight, velocity, function(){
+    heli.moveBezierToWithCallback(start.x + 1, start.y, zFlight, velocity, function(){
       heli.moveBezierCircle(0, 0, zFlight, radius, velocity);
       Space.schedule(function() {
         heli.moveBezierToObj(building, "Top", velocity, stopHeli);
