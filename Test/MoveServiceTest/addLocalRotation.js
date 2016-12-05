f9037d9e1e5360bb3de19d5272cfe5c8755cc6c5
Ruslan Zabrodin <ruslan@delightex.com>
2016-12-05 14:28:39 +0100

    create test package, add addLocalRotation and moveLinear test

---
+++ Test/MoveServiceTest/addLocalRotation.js
@@ -0,0 +1,15 @@
+var cube1 = Space.createItem("Cube", 0, 0, 0);
+var cube2 = Space.createItem("Cube", 0, 0, 0.5);
+var cube3 = Space.createItem("Cube", 0, 0, 1);
+
+Space.scheduleRepeating(function() {
+  cube1.addLocalRotation(0, 0, 0, 0, 0, 1, Math.PI / 200);
+  cube2.addLocalRotation(0, 0, 0, 0, 0, 1, -Math.PI / 50);
+  cube3.addLocalRotation(0, 0, 0, 0, 0, 1, 0);
+}, 0);
+
+var cube = Space.createItem('Cube', 2, -2, 1);
+
+Space.scheduleRepeating(function() {
+  cube.addLocalRotation(0, 0, 0, 1, 1, 1, 0.01);
+}, 0);
