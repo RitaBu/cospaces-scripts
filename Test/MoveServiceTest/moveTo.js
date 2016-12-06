14eb15ea1b63e7a5bec7cafd93ba395abf0b58c9
Ruslan Zabrodin <ruslan@delightex.com>
2016-12-06 12:23:55 +0100

    update

--- Test/MoveServiceTest/moveTo.js
+++ Test/MoveServiceTest/moveTo.js
@@ -1,19 +1,41 @@
 var radius = 5;
 var man = Space.createItem('LP_Man', 0, 0, radius);
-var woman = Space.createItem('LP_Wom', 0, 0, 1);
 var placeholder = Space.createItem("Sphere", 0, 0, 0);
 placeholder.setScale(0.5);
-placeholder.setColor("Blue");
+placeholder.setColor(0, 0, 255);
+man.say('MoveTo');
 
-move();
-woman.moveTo(-5, 5);
-
-function randomPosition(){
-  return {x: (Math.random() * 2 - 1) * radius, y: (Math.random() * 2 - 1) * radius, z: Math.random() * 2 * radius};
+function randomPosition() {
+  return {x: (Math.random() * 2 - 1) * radius, y: (Math.random() * 2 - 1) * radius, z: (Math.random() + 1) * radius};
 }
 
+move();
 function move() {
   var pos = randomPosition();
   placeholder.setPosition(pos.x, pos.y, pos.z);
   man.moveTo(pos.x, pos.y, pos.z, move);
 }
+
+var woman = Space.createItem('LP_Wom', 0, 0, 0);
+var placeholder2 = Space.createItem('Sphere', 0, 0, 0);
+placeholder2.setScale(0.5);
+placeholder2.setColor(255, 0, 0);
+
+moveToItem();
+function moveToItem() {
+  var pos = randomPosition();
+  placeholder2.setPosition(pos.x, pos.y, pos.z);
+  woman.say('MoveToItem');
+  var distance = woman.distanceToItem(placeholder2);
+  woman.moveToItem(placeholder2, distance, function () {
+    woman.say('Gotcha!!!');
+    Space.schedule(moveToItem, 1);
+  });
+}
+
+var placeholder3 = Space.createItem('Sphere', -10, 10, 1);
+placeholder3.setScale(0.5);
+placeholder3.setColor(0, 255, 0);
+var lion = Space.createItem('LP_Lion', -3, 1, 1);
+lion.say('moveTo(-10, 10)');
+lion.moveTo(-10, 10);
