73b4c39bd4569d2a541388b6fde60fb14b00d277
Ruslan Zabrodin <ruslan@delightex.com>
2016-12-06 09:03:28 +0100

    change addLocalPosition

--- Test/MoveServiceTest/addLocalPosition.js
+++ Test/MoveServiceTest/addLocalPosition.js
@@ -1,15 +1,26 @@
 var radius = 5;
 var cube = Space.createItem('Cube', 0, 0, 5);
-var placeholder = Space.createItem("Sphere", 0, 0, 5);
-placeholder.setScale(0.5);
-placeholder.setColor("Blue");
+var vector;
+var dir;
+var cnt = 0;
 
-Space.scheduleRepeating(move, 2);
+Space.scheduleRepeating(move, 1);
 
 function move() {
-  var posP = placeholder.getPosition();
   var posC = cube.getPosition();
-  cube.addLocalPosition(posP.x - posC.x, posP.y - posC.y, posP.z - posC.z);
-  var pos = {x: Math.random() * radius, y: Math.random() * radius, z: Math.random() * radius};
-  placeholder.setPosition(pos.x, pos.y, pos.z);
+  if (cnt % 3 === 0) {
+    dir = {
+      x: (Math.random() * 2 - 1) * radius,
+      y: (Math.random() * 2 - 1) * radius,
+      z: (Math.random() * 2 - 1) * radius
+    };
+    vector = Space.createVectorItem(posC.x, posC.y, posC.z, dir.x, dir.y, dir.z);
+  }
+  else if (cnt % 3 === 1) {
+    cube.addLocalPosition(dir.x, dir.y, dir.z);
+  }
+  else if (cnt % 3 === 2) {
+    vector.deleteFromSpace();
+  }
+  cnt += 1;
 }
