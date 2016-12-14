b9a519eee955b529c82dc2a5eaabc7ddcbc7b982
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-12-14 23:05:56 +0100

    test for rotation and moveLinear

--- Test/MoveServiceTest/addLocalRotation.js
+++ Test/MoveServiceTest/addLocalRotation.js
@@ -2,8 +2,8 @@
 var cube1 = Space.createItem("Cube", 0, 0, 0);
 var cube2 = Space.createItem("Cube", 0, 0, 0.5);
 var cube3 = Space.createItem("Cube", 0, 0, 1);
-cube2.setColor(255, 0, 0);
-cube3.setColor(255, 255, 0);
+cube2.setColor(255, 0, 0);   //red
+cube3.setColor(255, 255, 0); //yellow
 
 //TEST addLocalRotation FOR UNGROUPED OBJECTS
 Space.scheduleRepeating(function () {
@@ -11,6 +11,8 @@ Space.scheduleRepeating(function () {
   cube2.addLocalRotation(0, 0, 0, 0, 0, 1, -Math.PI / 100);
   cube3.addLocalRotation(0, 0, 0, 0, 0, 1, 0);
 }, 0);
+
+/////////////////////-------------////////////////////////
 
 var cubeGreen = Space.createItem("Cube", 4, 0, 0);
 cubeGreen.setColor(0, 255, 0); //green
@@ -25,7 +27,9 @@ var f = function () {
         });
     });
 };
-Space.schedule(f, 2);
+f();
+
+/////////////////////-------------////////////////////////
 
 //grouped
 var cubeWhite = Space.createItem("Cube", 2, 0, 0);
@@ -48,6 +52,41 @@ Space.scheduleRepeating(function () {
   cubeRed.addLocalRotation(0, 0, 0, 0, 0, 1, -Math.PI / 100);
   cubeYellow.addLocalRotation(0, 0, 0, 0, 0, 1, 0);
 }, 0);
+
+/////////////////////-------------////////////////////////
+
+//nested group
+var cube10 = Space.createItem("Cube", 8, 0, 0);
+
+var cube01 = Space.createItem("Cube", 7.5, 0, 0.5);
+var cube11 = Space.createItem("Cube", 8.0, 0, 0.5);
+var cube21 = Space.createItem("Cube", 8.5, 0, 0.5);
+
+cube10.setColor(50, 50, 50);
+cube01.setColor(100 + 20, 100, 100);
+cube11.setColor(120 + 20, 120, 120);
+cube21.setColor(160 + 20, 160, 160);
+
+//TEST setOrientationFrom
+var group0 = Space.createGroup();
+group0.add(cube01);
+group0.add(cube11);
+group0.add(cube21);
+group0.setOrientationFrom(cube11);
+
+var group1 = Space.createGroup();
+group1.add(group0);
+group1.add(cube10);
+group1.setOrientationFrom(cube10);
+
+//TEST addLocalRotation FOR GROUPED OBJECTS
+Space.scheduleRepeating(function () {
+  group0.addLocalRotation(0, 0, 0, 0, 0, 1, Math.PI / 100);
+  cube01.addLocalRotation(0, 0, 0.25, 1, 0, 0, Math.PI / 100);
+  cube21.addLocalRotation(0, 0, 0.25, 1, 0, 0, -Math.PI / 100);
+}, 0);
+
+/////////////////////-------------////////////////////////
 
 //second group
 var cubeBlue = Space.createItem("Cube", 6, 0, 0);
