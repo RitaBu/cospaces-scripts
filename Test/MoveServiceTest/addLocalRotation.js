6e57d319057c1490755c9fc13d13f91cce4cc65a
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-12-14 19:16:35 +0100

    test for local rotation

--- Test/MoveServiceTest/addLocalRotation.js
+++ Test/MoveServiceTest/addLocalRotation.js
@@ -5,7 +5,7 @@ var cube3 = Space.createItem("Cube", 0, 0, 1);
 cube2.setColor(255, 0, 0);
 cube3.setColor(255, 255, 0);
 
-//addLocalRotation FOR UNGROUPED OBJECTS
+//TEST addLocalRotation FOR UNGROUPED OBJECTS
 Space.scheduleRepeating(function () {
   cube1.addLocalRotation(0, 0, 0, 0, 0, 1, Math.PI / 200);
   cube2.addLocalRotation(0, 0, 0, 0, 0, 1, -Math.PI / 100);
@@ -15,17 +15,17 @@ Space.scheduleRepeating(function () {
 var cubeGreen = Space.createItem("Cube", 4, 0, 0);
 cubeGreen.setColor(0, 255, 0); //green
 
-//rotate & moveLinear
-Space.schedule(function () {
-  cubeGreen.rotate(0, 0, 1, Math.PI * 0.5, 2, function () {
+//TEST rotate & moveLinear
+var f = function () {
+  cubeGreen.rotate(0, 0, 1, Math.PI * 0.6, 2, function () {
     cubeGreen.moveLinear(4, 0, 3, 2, function () {
-      cubeGreen.rotate(0, 0, 1, Math.PI * 1.5, 2, function () {
-        cubeGreen.moveLinear(4, 0, 0, 2, function () {
+      cubeGreen.rotate(0, 0, 1, Math.PI * 1.4, 2, function () {
+        cubeGreen.moveLinear(4, 0, 0, 2, f);
+            });
         });
-      });
     });
-  });
-}, 2);
+};
+Space.schedule(f, 2);
 
 //grouped
 var cubeWhite = Space.createItem("Cube", 2, 0, 0);
@@ -34,6 +34,7 @@ var cubeYellow = Space.createItem("Cube", 2, 0, 1);
 cubeRed.setColor(255, 0, 0); //red
 cubeYellow.setColor(255, 255, 0); //yellow
 
+//TEST setOrientationFrom
 var group = Space.createGroup();
 group.add(cubeWhite);
 group.add(cubeRed);
@@ -41,12 +42,27 @@ group.add(cubeYellow);
 
 group.setOrientationFrom(cubeYellow);
 
-//addLocalRotation FOR GROUPED OBJECTS
+//TEST addLocalRotation FOR GROUPED OBJECTS
 Space.scheduleRepeating(function () {
   cubeWhite.addLocalRotation(0, 0, 0, 0, 0, 1, Math.PI / 200);
   cubeRed.addLocalRotation(0, 0, 0, 0, 0, 1, -Math.PI / 100);
   cubeYellow.addLocalRotation(0, 0, 0, 0, 0, 1, 0);
 }, 0);
+
+//second group
+var cubeBlue = Space.createItem("Cube", 6, 0, 0);
+var cubeGray = Space.createItem("Cube", 6, 0, 0.5);
+cubeBlue.setColor(0, 0, 255); //blue
+cubeGray.setColor(150, 150, 150); //gray
+
+var group2 = Space.createGroup();
+group2.add(cubeBlue);
+group2.add(cubeGray);
+
+group2.setOrientationFrom(cubeBlue);
+
+//TEST rotate FOR GROUP
+group2.rotate(0, 0, 1, Math.PI * 0.6, 2, null);
 
 /*
 //move linear
