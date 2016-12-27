5ca5065437af3057a85095f3fa28a42495d9443f
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-12-27 18:02:08 +0100

    script for tower

--- Test/MoveServiceTest/tower.js
+++ Test/MoveServiceTest/tower.js
@@ -1,16 +1,37 @@
-var prev = null;
+//CUBE TOWER
+{
+    var prev = null;
 
-var scale = 5;
-for (var i = 0; i < 10; i++) {
-    var cube = Space.createItem("Cube", 0, 0, 0);
-    cube.setScale(scale);
-    cube.setRandomColor();
+    var scale = 5;
+    for (var i = 0; i < 10; i++) {
+        var cube = Space.createItem("Cube", 0, 0, 0);
+        cube.setScale(scale);
+        cube.setRandomColor();
 
-    scale = scale * 0.8;
-    if (prev != null) {
-        cube.adjustTo("bottom", prev, "top");
+        scale = scale * 0.8;
+        if (prev != null) {
+            cube.adjustTo("bottom", prev, "top");
+        }
+        prev = cube;
     }
-    prev = cube;
+}
+
+//CHIP TOWER
+{
+    var prev = null;
+
+    var scale = 5;
+    for (var i = 0; i < 30; i++) {
+        var cube = Space.createItem("CheckerChip", 10, 0, 0);
+        cube.setScale(scale);
+        cube.setRandomColor();
+
+        scale = scale * 0.9;
+        if (prev != null) {
+            cube.adjustTo("bottom", prev, "top");
+        }
+        prev = cube;
+    }
 }
 
 
