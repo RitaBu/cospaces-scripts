0dc8c1425c36bf6d71c51f8c684946f9c1fc22fd
Benjamin Singh <bfranklin1158@gmail.com>
2016-12-28 15:39:34 +0100

    Refactor and fix ZombieGame.

--- ZombieGame/README.md
+++ ZombieGame/README.md
@@ -3,16 +3,13 @@
 > Zombies will attack you from everywhere. Kill them!
 
 ## Project location
-
-[dev https://newcospaces.dx.labs.intellij.net/shorten/dmof](https://newcospaces.dx.labs.intellij.net/shorten/dmof)  
-[v0.3 https://cospac.es/aa8a](https://cospac.es/aa8a)  
-[v0.2 https://cospac.es/jAQC](https://cospac.es/jAQC)
+    
+[https://cospac.es/1hKE](https://cospac.es/1hKE)
 
 ## How to play the game
 
-1. Zombies will appear one after another to hunt you.
+1. Go into VR or gyroscope mode.
 2. You play in first person perspective.
-3. You can run away by tapping on the screen (VR/Gyroscope mode) or by stroking the arrow keys on your keyboard.
+3. Zombies will appear around you one after another.
 4. To kill a zombie tap/click on it.
-5. Don't let the zombies come close to you or you will die!
-6. Kill as many zombies as you can!
+5. Kill as many zombies as you can!
--- ZombieGame/zombieGame.js
+++ ZombieGame/zombieGame.js
@@ -1,164 +1,138 @@
-/********** Game **********/
-function Game(config) {
-  this.enemyModelId = config.enemyModelId;
-  this.enemyModel2Id = config.enemyModel2Id;
-  this.enemySpawnRadius = config.enemySpawnRadius;
-  this.enemySpawnInterval = config.enemySpawnInterval;
-  this.gameOverScreen = config.gameOverScreen;
-  this.playerCanDie = config.playerCanDie;
-  this.soundClip = Space.loadSound('fd41215166bfb85049288d7c3d1cc4db68e8af0f805b0a5a9a36059a26adb3d3');
-  this.soundZombieHorde = Space.loadSound('5b5dce36e80b678a17cc5c8cff2a7426f2df55ab2fbc81c64978eb5ed554bfce');
-  this.player = {};
-  this.enemies = [];
-  this.enemyIdCounter = 0;
+/**
+ * Helper methods
+ *
+ */
+var Helper = {
+  randNumBetween: function(min, max) {
+    return Math.random() * (max - min) + min;
+
+  },
+  inheritsFrom: function(child, parent) {
+    child.prototype = Object.create(parent.prototype);
+  }
+};
+
+/**
+ * Player constructor function
+ *
+ */
+function Player() {
+  this.item = Space.createItem('LP_Wom', 0, 0, 0);
+  this.init();
 }
 
-Game.prototype.playMusic = function() {
-  this.soundClip.play(true);
-  this.soundZombieHorde.setVolume(0.2);
-  this.soundZombieHorde.play(true);
+Player.prototype.init = function() {
+  this.item.focusOn(true);
 };
 
-Game.prototype.spawnPlayerAt = function(xPos, yPos) {
-  this.player = new Player(Space.createItem('LP_Man', xPos, yPos, 0), 10);
-};
+/**
+ * Zombie constructor function
+ *
+ */
+function Zombie() {
+  this.item = {};
+  this.modelId = '%%370077d9c1b39305ae1b1989393e132e67259707eb06b3c17c5d07802d1e47ea';
+  this.groanSound = Space.loadSound('fa2832d97694e4650c1ecd6e6ade545a3fc41ff82f50d5e8a3a826015034f494');
+}
 
-Game.prototype.spawnZombie = function() {
-  var posXY = this.generateEnemySpawnPos(this.enemySpawnRadius);
-  var posZ = 0;
-
-  var newZombie = new Zombie(Space.createItem(this.enemyModelId, posXY.x, posXY.y, posZ), this.enemyIdCounter, 'zombie');
-  newZombie.initAttack();
-
-  newZombie.gameItem.onActivate(function() {
-    game.player.killEnemy(newZombie);
+Zombie.prototype.bindEvents = function() {
+  var self = this;
+  this.item.onActivate(function() {
+    self.remove();
   });
-
-  this.enemies.push(newZombie);
-  this.enemyIdCounter++;
+  return this;
 };
 
-Game.prototype.spawnSkull = function() {
-  var posXY = this.generateEnemySpawnPos(this.enemySpawnRadius);
-  var posZ = Math.random() * (5 - 1) + 1;
-
-  var newSkull = new Skull(Space.createItem(this.enemyModel2Id, posXY.x, posXY.y, posZ), this.enemyIdCounter, 'skull');
-  newSkull.initAttack();
-
-  newSkull.gameItem.onActivate(function() {
-    game.player.killEnemy(newSkull);
-  });
-
-  this.enemies.push(newSkull);
-  this.enemyIdCounter++;
-};
-
-Game.prototype.generateEnemySpawnPos = function(radius) {
-  var a = Math.random() * (2 * Math.PI);
-  var cx = 0;
-  var cy = 0;
-
-  var xPos = cx + radius * Math.cos(a);
-  var yPos = cy + radius * Math.sin(a);
+Zombie.prototype.getSpawnPos = function() {
+  var a = Helper.randNumBetween(0, Math.PI * 2);
+  var radius = 15;
 
   return {
-    x: xPos,
-    y: yPos
+    x: Math.cos(a) * radius,
+    y: Math.sin(a) * radius,
+    z: 0
   };
 };
 
-/********** Zombie **********/
-function Zombie(gameItem, id, type) {
-  this.gameItem = gameItem;
-  this.id = id;
-  this.type = type;
-  this.dyingSound = Space.loadSound('fa2832d97694e4650c1ecd6e6ade545a3fc41ff82f50d5e8a3a826015034f494');
-}
-
-Zombie.prototype.initAttack = function() {
-  this.gameItem.faceTo(game.player.gameItem);
-  this.gameItem.playIdleAnimation('Walk');
-  this.followPlayer();
+Zombie.prototype.spawn = function() {
+  var spawnPos = this.getSpawnPos();
+  this.item = Space.createItem(this.modelId, spawnPos.x, spawnPos.y, spawnPos.z);
+  this.item.faceTo(player.item);
+  this.item.playIdleAnimation('Walk');
+  return this;
 };
 
 Zombie.prototype.followPlayer = function() {
-  var distanceToPlayer = this.gameItem.distanceToItem(game.player.gameItem) + 1;
-  this.gameItem.moveLinear(game.player.gameItem.getPosition().x, game.player.gameItem.getPosition().y, game.player.gameItem.getPosition().z, distanceToPlayer);
+  var playerPos = player.item.getPosition();
+  var distanceToPlayer = this.item.distanceToItem(player.item);
+  this.item.moveLinear(playerPos.x, playerPos.y, playerPos.z, distanceToPlayer + 0.5);
+  return this;
 };
 
-Zombie.prototype.hurtPlayer = function() {
-  if (this.gameItem.distanceToItem(game.player.gameItem) < 2) {
-    this.gameItem.deleteFromSpace();
+Zombie.prototype.checkLimits = function() {
+  var distanceToPlayer = this.item.distanceToItem(player.item);
+  if(distanceToPlayer < 1) {
+    this.remove();
   }
+  return this;
 };
 
-/********** Skull **********/
-function Skull(gameItem, id, type) {
-  this.gameItem = gameItem;
-  this.id = id;
-  this.type = type;
-  this.dyingSound = Space.loadSound('24777a1e286dbcae98d5593fd9c4bc6bbde60dba8a697fea85d94b961feac0a5');
+Zombie.prototype.remove = function() {
+  this.groanSound.play();
+  var index = enemies.indexOf(this);
+  enemies.splice(index, 1);
+  this.item.deleteFromSpace();
+};
+
+/**
+ * Skull constructor function
+ *
+ */
+
+function Skull() {
+  this.item = {};
+  this.modelId = '%%abaf39a9e30c47b817a9bca6735591c3a840312b9ada82e4a0bb8bcaf36eb5ab';
+  this.groanSound = Space.loadSound('24777a1e286dbcae98d5593fd9c4bc6bbde60dba8a697fea85d94b961feac0a5');
 }
 
-Skull.prototype.initAttack = function() {
-  this.gameItem.faceTo(game.player.gameItem);
-  this.gameItem.playIdleAnimation('Clicking');
-  this.followPlayer();
+Helper.inheritsFrom(Skull, Zombie);
+
+Skull.prototype.spawn = function() {
+  var spawnPos = this.getSpawnPos();
+  this.item = Space.createItem(this.modelId, spawnPos.x, spawnPos.y, 3);
+  this.item.faceTo(player.item);
+  this.item.playIdleAnimation('Clicking');
+  return this;
 };
 
-Skull.prototype.followPlayer = function() {
-  var distanceToPlayer = this.gameItem.distanceToItem(game.player.gameItem) + 1;
-  this.gameItem.moveLinear(game.player.gameItem.getPosition().x, game.player.gameItem.getPosition().y, game.player.gameItem.getPosition().z, distanceToPlayer);
-};
+/**
+ * Init
+ *
+ */
+Space.loadSound('fd41215166bfb85049288d7c3d1cc4db68e8af0f805b0a5a9a36059a26adb3d3').play(true);
+Space.loadSound('5b5dce36e80b678a17cc5c8cff2a7426f2df55ab2fbc81c64978eb5ed554bfce').play(true);
 
-Skull.prototype.hurtPlayer = function() {
-  if (this.gameItem.distanceToItem(game.player.gameItem) < 2) {
-    this.gameItem.deleteFromSpace();
-  }
-};
-
-/********** Player **********/
-function Player(gameItem) {
-  this.gameItem = gameItem;
-}
-
-Player.prototype.killEnemy = function(enemy) {
-  enemy.dyingSound.play();
-  enemy.gameItem.deleteFromSpace();
-
-  var index = game.enemies.indexOf(enemy);
-  if (index !== -1) {
-    game.enemies.splice(index, 1);
-  }
-};
-
-/********** Init **********/
-Space.clear();
-
-var gameConfig = {
-  enemyModelId: '%%370077d9c1b39305ae1b1989393e132e67259707eb06b3c17c5d07802d1e47ea', // Zombie
-  enemyModel2Id: '%%abaf39a9e30c47b817a9bca6735591c3a840312b9ada82e4a0bb8bcaf36eb5ab', // Skull
-  enemySpawnRadius: 15,
-  enemySpawnInterval: 5
-};
-var game = new Game(gameConfig);
-game.playMusic();
-
-game.spawnPlayerAt(0, 0);
-game.player.gameItem.focusOn(true);
-
-Space.scheduleRepeating(function() {
-  game.spawnZombie();
-  game.spawnSkull();
-  game.enemies.forEach(function(enemy) {
-    enemy.followPlayer();
-    enemy.hurtPlayer();
-  });
-}, game.enemySpawnInterval);
-
+var player = new Player();
+var enemies = [];
 var mood = 1;
-Space.setMood(mood);
+
 Space.scheduleRepeating(function() {
-  mood -= 0.001;
-  Space.setMood(mood);
+  enemies.forEach(function(enemy) {
+    enemy.followPlayer().checkLimits();
+  });
+
+  if(mood > 0) {
+    mood -= 0.001;
+    Space.setMood(mood);
+  }
 }, 1 / 15);
+
+Space.scheduleRepeating(function() {
+  var zombie = new Zombie();
+  var skull = new Skull();
+
+  zombie.spawn().bindEvents();
+  skull.spawn().bindEvents();
+
+  enemies.push(zombie, skull);
+}, 5);
