1a821c776b57da18440e3f54d057652e1847c93a
Michael Medvinskiy <Michael.Medvinskiy@jetbrains.com>
2016-12-29 17:32:55 +0100

    moveBezierToObj implemented with PathActor

--- Test/MoveServiceTest/moveBezierToObj.js
+++ Test/MoveServiceTest/moveBezierToObj.js
@@ -5,7 +5,6 @@ function randomPosition() {
 var radius = 5;
 
 var woman = Space.createItem('LP_Wom', 0, 0, radius);
-woman.say('moveBezierToObj');
 var target2 = Space.createItem("Sphere", 0, 0, 0);
 target2.setScale(0.5);
 target2.setColor(255, 0, 0);
@@ -14,5 +13,9 @@ moveToObj();
 function moveToObj() {
   var pos = randomPosition();
   target2.setPosition(pos.x, pos.y, pos.z);
-  woman.moveBezierToObj(target2, '', radius / 2, moveToObj);
+  woman.say('moveBezierToObj');
+  woman.moveBezierToObj(target2, '', radius / 2, function() {
+    woman.say('rotateLocal');
+    woman.rotateLocal(0, 1, 0, Math.PI * 0.25 * (1 + Math.random()), 1, moveToObj);
+  });
 }
