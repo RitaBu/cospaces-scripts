269a50cbea06007b23dafb726741064fa9a20b2f
Benjamin Singh <bfranklin1158@gmail.com>
2017-02-28 10:08:31 +0100

    Demo Space.copyItem(), item.rotateAs().

---
+++ GraphicsDemos/compose/README.md
@@ -0,0 +1,3 @@
+## Project location
+
+[https://cospac.es/NUPJ](https://cospac.es/NUPJ)
---
+++ GraphicsDemos/compose/compose.js
@@ -0,0 +1,25 @@
+var items = Space.getItems();
+
+var itemsCopy = items.map(function (item) {
+  item.setOpacity(0);
+  return Space.copyItem(item);
+});
+
+items.forEach(function (item, index) {
+  var xPos = Math.random() * 60 - 30;
+  var yPos = Math.random() * 60 - 30;
+  var angle = Math.random() * Math.PI * 2;
+
+  item.setOpacity(1);
+  item.addLocalRotation(0, 0, 0, 0, 0, 1, angle);
+  item.setPosition(xPos, yPos, 0);
+
+  (function (item, index) {
+    var itemCopy = itemsCopy[index];
+    var itemCopyPos = itemCopy.getPosition();
+    item.moveTo(itemCopyPos.x, itemCopyPos.y, itemCopyPos.z, function () {
+      var time = Math.random() * 2 + 3;
+      item.rotateAs(itemCopy, time);
+    });
+  })(item, index);
+});
